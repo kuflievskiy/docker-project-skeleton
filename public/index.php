d1f9e2bf392358f7cb9379b7986ddc9446d43aa6
Aleksey Kuflievskiy <kuflievsky@kuflievsky-lx.nixsolutions.com>
2019-04-09 11:25:59 +0300

    Added sSMTP service to the webserver container and it`s configuration file, modified template file

--- public/index.php
+++ public/index.php
@@ -1,22 +1,115 @@
-<?php
-//phpinfo();
+<html>
+<head>
+    <title>Docker based project skeleton</title>
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<!-- Compiled and minified JavaScript -->
+	<script src="//cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+    <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col s12">
+                    <h1>Docker based project skeleton <i class="material-icons">directions_bike</i></h1>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col s12">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>PHP</td>
+                                <td><?php echo phpversion(); ?></td>
+                            </tr>
+                            <tr>
+                                <td>MySQL</td>
+                                <td>
+                                    <?php
+                                    $mysqli = new mysqli('container_dbhost', 'dbuser', 'dbpass', 'dbname');
 
-echo "PHP Version: " . phpversion() . '<br>';
-
-// Connecting to and selecting a MySQL database
-$mysqli = new mysqli('container_dbhost', 'dbuser', 'dbpass', 'dbname');
-
-// Oh no! A connect_errno exists so the connection attempt failed!
-if ($mysqli->connect_errno) {
-	// Let's try this:
-	echo "Sorry, this website is experiencing problems.";
-	// Something you should not do on a public site, but this example will show you
-	// anyways, is print out MySQL error related information -- you might log this
-	echo "Error: Failed to make a MySQL connection, here is why: \n";
-	echo "Errno: " . $mysqli->connect_errno . "\n";
-	echo "Error: " . $mysqli->connect_error . "\n";
-	// You might want to show them something nice, but we will simply exit
-	exit;
-} else {
-	echo "MYSQL Client info: " . $mysqli->client_info . '<br>';
-}
+                                    if ($mysqli->connect_errno) {
+                                        echo "Errno: " . $mysqli->connect_errno . "<br>" . $mysqli->connect_error . "\n";
+                                    } else {
+                                        echo $mysqli->client_info;
+                                    }
+                                    ?>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12">
+                    <h2>Send test email:</h2>
+	                <?php
+                        if ( 'send_email' == filter_input( INPUT_POST, 'send_email', FILTER_SANITIZE_STRING ) ) {
+                            $email           = filter_input( INPUT_POST, 'email', FILTER_SANITIZE_STRING );
+                            $email_subject   = filter_input( INPUT_POST, 'email_subject', FILTER_SANITIZE_STRING );
+                            $email_body      = filter_input( INPUT_POST, 'email_body', FILTER_SANITIZE_STRING );
+                            $sendmail_result = mail( $email, $email_subject, $email_body );
+                            if ( $sendmail_result ) {
+                                ?><p>Test mail has been sent successfully!</p><?php
+                            } else {
+                                ?><p><b>Error!</b> Test mail has <b>not</b> been sent!</p><?php
+                            }
+                        }
+	                ?>
+                    <form action="#" method="post">
+                        <input type="hidden" name="send_email" value="send_email">
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <label for id="email">Email to:</label>
+                                <input id="email" type="email" name="email" class="validate" required value="">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <label for="email_subject">Email Subject:</label>
+                                <input id="email_subject" type="text" name="email_subject" class="validate" required value="Test subject">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <label for="email_body">Email Body:</label>
+                                <textarea id="email_body" name="email_body" class="materialize-textarea validate" required>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</textarea>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                                <i class="material-icons right">send</i>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer class="">
+        <div class="container">
+            <div class="row">
+                <div class="col s12">
+                    <div class="copyright-text">
+                        <p><center>Copyright ©<?php echo date( 'Y' ); ?> Developed by <a href="https://github.com/kuflievskiy" target="_blank" class="text-danger">Kuflievskiy Aleksey</a></center></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
--- readme.md
+++ readme.md
@@ -1,12 +1,12 @@
-# Instructions
+# Instructions.
 
-## Step 0. Install docker and docker-compose
+## Install docker and docker-compose.
 
 ```bash
 sudo apt-get install docker docker-compose -y
 ```
 
-## Step 1. In order to build the env for this project from scratch, 
+## In order to build the env for this project from scratch, 
 # Remove the containers, images, volume from the local machine.
 # You can clear all the docker stuff: containers, images and it's data using these commands:
 
@@ -17,6 +17,7 @@ sudo docker volume rm $(sudo docker volume ls -q)
 ```
 
 # Add your user to docker group:
+
 ```
 sudo usermod -aG docker $USER
 ```
@@ -29,24 +30,33 @@ docker rmi $(docker images -q) && \
 docker volume rm $(docker volume ls -q)
 ```
 
-## Step 2. Build images & run the containers
+## Configure sSMTP connection via Google SMTP (Optional).
+
+```bash
+cp docker/webserver/etc/ssmtp/ssmtp.conf.sample docker/webserver/etc/ssmtp/ssmtp.conf
+vim docker/webserver/etc/ssmtp/ssmtp.conf
+```
+
+## Build images & run the containers.
+
 ```bash
 sudo docker-compose up -d --build
 ```
 
-## Step 3. Access the site by URL
+## Access the site by URL.
 
 - Dev site URL: http://localhost:8080/
 - PHPMyAdmin: http://localhost:8181/
 
-## Step 4. Login into the container if need to apply some changes to the server configuration
+## Login into the container if need to apply some changes to the server configuration.
 
 ```bash
 sudo docker exec -it container_webserver bin/bash
 sudo docker exec -it container_dbhost bin/bash
 ```
 
-# get the list of active processes inside the container which listen to some port
+# Get the list of active processes inside the container which listen to some port.
+
 ```bash
 netstat -an|grep LISTEN
 ```
